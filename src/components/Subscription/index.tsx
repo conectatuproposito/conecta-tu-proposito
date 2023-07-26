1a1c981f93a786ad15ca8a239768a7f3d579e91b
revione <revione@outlook.com>
2023-07-26 09:21:02 +0200

    vamo

--- src/components/Subscription/index.tsx
+++ src/components/Subscription/index.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import { FC, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import Button from "@/components/Button";
 import Image from "next/image";
 
 export type FormData = {
   email: string;
 };
 
-const Subscription: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+const Subscription = () => {
+  const { register, handleSubmit, setValue } = useForm<FormData>();
   const [enviado, setEnviado] = useState(false);
   const [enviando, setEnviando] = useState(false);
   const [error, setError] = useState(false);
@@ -27,17 +26,19 @@ const Subscription: FC = () => {
       .then((res) => {
         console.log({ res });
         setEnviado(true);
+        setValue("email", "Bienvenido ahora estas conectado");
       })
       .catch((err) => {
         console.log({ err });
-        setError(err);
+        setError(true);
+        setValue("email", "Algo salio mal, algo salio mal");
       })
       .finally(() => {
         setEnviando(false);
       });
   }
 
-  const Form = () => (
+  return (
     <form className="max-w-3xl m-auto" onSubmit={handleSubmit(onSubmit)}>
       {error && <div>{error}</div>}
 
@@ -49,14 +50,24 @@ const Subscription: FC = () => {
           <input
             type="email"
             placeholder="example@domain.com"
-            className=" h-8 w-full border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md rounded-s-[30px] transition-all"
+            className={`
+              h-8 w-full border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md rounded-s-[30px] 
+              ${enviado && "text-[#3ABBED] uppercase"} ${
+              error && "text-[#E73D5C] uppercase"
+            } 
+              transition-all
+            `}
             {...register("email", { required: true })}
           />
         </div>
 
         <button
           disabled={enviando}
-          className="h-8 w-8 self-end rounded-e-[30px] flex items-center text-white  bg-violet-950  hover:bg-sky-400 transition-all "
+          className={`
+            h-8 w-8 self-end rounded-e-[30px] flex items-center text-white hover:bg-sky-400 transition-all 
+            ${!(enviado || error) ? "bg-[#6D2EA3]" : ""}
+             ${enviado ? "bg-[#3ABBED]" : ""} ${error ? "bg-[#E73D5C]" : ""}
+          `}
         >
           <Image
             className="pt-[1px] ml-2 w-3 h-3"
@@ -69,19 +80,6 @@ const Subscription: FC = () => {
         </button>
       </div>
     </form>
-  );
-
-  return (
-    <div>
-      {enviado ? (
-        <div>
-          Gracias por contactar con nosotros, dentro de poco estaremos en
-          contacto con tigo.
-        </div>
-      ) : (
-        <Form />
-      )}
-    </div>
   );
 };
 
